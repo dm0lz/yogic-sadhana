78457588a612936dbab291d0eea15cf4de8f3914
oducroux <ducrouxolivier@gmail.com>
2015-02-07 21:36:17 +0100

    fixing assets

--- app/assets/javascripts/frontend/factories.js
+++ app/assets/javascripts/frontend/factories.js
@@ -1,5 +1,5 @@
 
-YsApp.factory('Courses', function($http){
+YsApp.factory('Courses', ['$http', function($http){
   var Courses = {};
   Courses.get = function(locale, callback){
     $http.get('/' + locale + '/api/v1/courses').success(function(data){
@@ -7,9 +7,9 @@ YsApp.factory('Courses', function($http){
     });
   };
   return Courses;
-});
+}]);
 
-YsApp.factory('Course', function($http){
+YsApp.factory('Course', ['$http', function($http){
   var Course = {};
   Course.get = function(locale, id, callback){
     $http.get('/' + locale + '/api/v1/courses/' + id).success(function(data){
@@ -17,7 +17,7 @@ YsApp.factory('Course', function($http){
     });
   };
   return Course;
-});
+}]);
 //
 // YsApp.factory('Chapter', function($http, $q){
 //   function Chapter(chapter){
@@ -42,7 +42,7 @@ YsApp.factory('Course', function($http){
 //   return Chapter;
 // });
 
-YsApp.factory('Chapter', function($http){
+YsApp.factory('Chapter', ['$http', function($http){
   var Chapter = {};
   Chapter.get = function(locale, id, callback){
     $http.get('/' + locale + '/api/v1/chapters/' + id).success(function(data){
@@ -50,10 +50,10 @@ YsApp.factory('Chapter', function($http){
     });
   };
   return Chapter;
-});
+}]);
 
 
-YsApp.factory('Practice', function($http){
+YsApp.factory('Practice', ['$http', function($http){
   var Practice = {};
   Practice.get = function(locale, id, callback){
     $http.get('/' + locale + '/api/v1/practices/' + id).success(function(data){
@@ -61,9 +61,9 @@ YsApp.factory('Practice', function($http){
     });
   };
   return Practice;
-});
+}]);
 
-YsApp.factory('Theory', function($http){
+YsApp.factory('Theory', ['$http', function($http){
   var Theory = {};
   Theory.get = function(locale, id, callback){
     $http.get('/' + locale + '/api/v1/theories/' + id).success(function(data){
@@ -71,9 +71,9 @@ YsApp.factory('Theory', function($http){
     });
   };
   return Theory;
-});
+}]);
 
-YsApp.factory('GetTheoryMedia', function($http){
+YsApp.factory('GetTheoryMedia', ['$http', function($http){
   var GetTheoryMedia = {};
   GetTheoryMedia.get = function(locale, id, callback){
     $http.get('/' + locale + '/api/v1/medias/' + id).success(function(data){
@@ -81,9 +81,9 @@ YsApp.factory('GetTheoryMedia', function($http){
     });
   };
   return GetTheoryMedia;
-});
+}]);
 
-YsApp.factory('GetPracticeMedia', function($http){
+YsApp.factory('GetPracticeMedia', ['$http', function($http){
   var GetPracticeMedia = {};
   GetPracticeMedia.get = function(locale, id, callback){
     $http.get('/' + locale + '/api/v1/medias/' + id).success(function(data){
@@ -91,7 +91,7 @@ YsApp.factory('GetPracticeMedia', function($http){
     });
   };
   return GetPracticeMedia;
-});
+}]);
 
 
 
--- app/assets/javascripts/frontend/services.js
+++ app/assets/javascripts/frontend/services.js
@@ -1,20 +1,20 @@
 
 /* SERVICES  */
-YsApp.service('Seeker', [function($event){
-  this.seekPercentage = function ($event) {
-    var percentage = ($event.offsetX / $event.target.offsetWidth);
-    if (percentage <= 1) {
-      return percentage;
-    } else {
-      return 0;
-    }
-  };
-}]);
+// YsApp.service('Seeker', [function($event){
+//   this.seekPercentage = function ($event) {
+//     var percentage = ($event.offsetX / $event.target.offsetWidth);
+//     if (percentage <= 1) {
+//       return percentage;
+//     } else {
+//       return 0;
+//     }
+//   };
+// }]);
 
-YsApp.service('Locale', function($location){
+YsApp.service('Locale', ['$location', function($location){
   this.getLocale = function(){
     var url = $location.$$absUrl;
     var locale = url.split("/")[3];
     return locale;
   };
-});
+}]);
