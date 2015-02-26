6b4db3c2edab0f41072669ae5258e9ba866ce898
oducroux <ducrouxolivier@gmail.com>
2015-02-26 22:14:21 +0100

    sub

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -3,3 +3,19 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require lib/jquery.colorbox
+
+
+$(document).bind('cbox_complete', function(){
+  $('form').submit(function() {
+    $("section.well.home").hide();
+    $(".register_message").show();
+  });
+});
+
+$(document).ready(function(){
+  $(".arrow-span").bind('click', function(){
+    $('html,body').animate({
+      scrollTop: window.scrollY + window.innerHeight
+    }, 1000);
+  });
+});
